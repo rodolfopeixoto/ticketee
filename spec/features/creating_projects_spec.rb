e76c36e03eb325cd8d5a6895881bd69f14e5f368
Rodolfo Peixoto <rdxinho@gmail.com>
2017-12-20 11:17:31 -0200

    Add title functionality for project show page

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -2,14 +2,21 @@ require 'rails_helper'
 
 RSpec.feature 'Users can create new projects' do
   scenario do
+    name = 'Rodolfo' # Faker::Name.name
     visit '/'
 
     click_link 'New Project'
 
-    fill_in 'Name', with: Faker::Name.name 
+    fill_in 'Name', with: name 
     fill_in 'Description', with: Faker::Lorem.paragraph(2)
     click_button 'Create Project'
 
     expect(page).to have_content 'Project has been created.'
+
+    project = Project.find_by(name: name)
+    expect(page.current_url).to eq project_url(project)
+
+    title = "#{project.name} - Projects - Ticketee"
+    expect(page).to have_title title
   end
 end
