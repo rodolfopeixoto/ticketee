043a0307847fdbd170a792ff185d2857c76616fe
Rodolfo Peixoto <rdxinho@gmail.com>
2018-01-02 13:43:39 -0200

    Don't list projects that a user doesn't have permission to see

--- spec/policies/project_policy_spec.rb
+++ spec/policies/project_policy_spec.rb
@@ -40,4 +40,29 @@ describe ProjectPolicy do
       expect(subject).not_to permit(user, project)
     end
   end
+
+  context "policy_scope" do
+    subject { Pundit.policy_scope(user, Project) }
+
+    let!(:project) { create :project }
+    let!(:user) { create :user }
+
+    it 'is empty for anonymous users' do
+      expect(Pundit.policy_scope(nil, Project)).to be_empty
+    end
+
+    it 'includes projects a user is allowed to view' do
+      assign_role!(user, :viewer, project)
+      expect(subject).to include(project)
+    end
+
+    it "doesn't include projects a user is not allowed to view" do
+      expect(subject).to be_empty
+    end
+
+    it 'return all projects for admins ' do
+      user.admin = true
+      expect(subject).to include(project)
+    end
+  end
 end
