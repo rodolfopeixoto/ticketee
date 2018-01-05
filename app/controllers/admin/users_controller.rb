bea16e50760d0123ff4a233ce4a7447d70e06959
Rodolfo Peixoto <rdxinho@gmail.com>
2018-01-05 13:23:49 -0200

    Roles can also be assigned when admins create new users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,7 +15,7 @@ class Admin::UsersController < Admin::ApplicationController
 
   def create
     @user = User.new(user_params)
-
+    build_roles_for(@user)
     if @user.save
       flash[:notice] = 'User has been created.'
       redirect_to admin_users_path
@@ -36,11 +36,7 @@ class Admin::UsersController < Admin::ApplicationController
     User.transaction do
       @user.roles.clear
       role_data = params.fetch(:roles, [])
-      role_data.each do |project_id, role_name|
-        if role_name.present?
-          @user.roles.build(project_id: project_id, role: role_name)
-        end
-      end
+      build_roles_for(@user)
     end
 
     if @user.update(user_params)
@@ -75,6 +71,15 @@ class Admin::UsersController < Admin::ApplicationController
 
     def set_project
       @projects = Project.order(:name)
+    end
+    
+    def build_roles_for(user)
+      role_data = params.fetch(:roles, [])
+      role_data.each do |project_id, role_name|
+        if role_name.present?
+          @user.roles.build(project_id: project_id, role: role_name)
+        end
+      end
     end
 
 end
